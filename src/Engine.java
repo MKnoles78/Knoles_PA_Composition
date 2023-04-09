c553c8b2a7fc3e857b49823a2574dc233da2c0dc
Mike Knoles <MKnoles78@gmail.com>
2023-04-09 19:26:24 -0400

    coded Engine class

--- src/Engine.java
+++ src/Engine.java
@@ -7,4 +7,33 @@ public class Engine {
     private int Cylinders;
     private String GasType;
     private boolean FuelInjected;
+
+    public Engine(int cylinders, String gasType, boolean fuelInjected) {
+        Cylinders = cylinders;
+        GasType = gasType;
+        FuelInjected = fuelInjected;
+    }
+
+    //getters and setters
+    public int getCylinders() {
+        return Cylinders;
+    }
+    public void setCylinders(int cylinders) {
+        Cylinders = cylinders;
+    }
+    
+    public String getGasType() {
+        return GasType;
+    }
+    public void setGasType(String gasType) {
+        GasType = gasType;
+    }
+
+    public boolean isFuelInjected() {
+        return FuelInjected;
+    }
+    public void setFuelInjected(boolean fuelInjected) {
+        FuelInjected = fuelInjected;
+    }
+
 }
